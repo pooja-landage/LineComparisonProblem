095b9e52649c5d1f35932a30596e744107402723
pooja-landage <poojalandage22@gmail.com>
2021-11-19 12:20:56 +0530

    [Pooja] : Added UC1

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//using System.Math;
+using static System.Math;
+
+namespace MathClass
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            //user Input
+            Console.Write("Enter any  value x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double distance = Convert.ToDouble(Console.Read());
+
+            distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine(distance);
+            Console.ReadKey();
+        }
+    }
+
+
+}
